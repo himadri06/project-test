7e9612c945ba3c3033b387c8f0938cb4354260bf
himadri06 <hbhowmick91@gmail.com>
2025-11-23 19:19:18 +0530

    Wheel file6

--- app.py
+++
@@ -1,6 +0,0 @@
-def addition(a, b):
-    return a + b
-def multiply(a, b):
-    return a * b
-print("I have cloned this repo in my local computer. Now i am developing the code")
-print("This is the CI commit")
